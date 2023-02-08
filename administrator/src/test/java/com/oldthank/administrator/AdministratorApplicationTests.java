38a7bbb4c05204708c6717730c53213dd99fc3ee
oldthank <404290996@qq.com>
2023-02-08 21:08:04 +0800

    初始化项目

---
+++ administrator/src/test/java/com/oldthank/administrator/AdministratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oldthank.administrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
